91375761b14d183e19c17d518ad911a318c54733
dnivedha <nivethadurairaju95@gmail.com>
2016-07-10 13:02:47 +0530

    finish

---
+++ lll.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+void tower(int, char, char, char);
+ 
+int main()
+{
+    int n;
+    printf("Enter the number of disks : ");
+    scanf("%d", &n);
+    printf("The seqof moves  :\n");
+    tower(n, 'A', 'C', 'B');
+    return 0;
+}
+void tower(int n, char frompeg, char topeg, char auxpeg)
+{
+    if (n == 1)
+    {
+        printf("\n Move disk 1 from peg %c to peg %c", frompeg, topeg);
+        return;
+    }
+    tower(n - 1, frompeg, auxpeg, topeg);
+    printf("\n Move disk %d from peg %c to peg %c", n, frompeg, topeg);
+    tower(n - 1, auxpeg, topeg, frompeg);
+    getch();
+}
